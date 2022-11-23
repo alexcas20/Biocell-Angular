2f9e10fe91d10ac72e18bb43526619ba19f58789
alexcas20 <alexcalvillosalas@gmail.com>
2022-11-22 23:11:49 -0600

    se agrego un folio dinamico, como cambios en la funcion del update

--- src/app/pages/dialog-pacientes/dialog-pacientes.component.ts
+++ src/app/pages/dialog-pacientes/dialog-pacientes.component.ts
@@ -1,10 +1,10 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import Register from 'src/app/models/register.interface';
+import RegistroPaciente from 'src/app/models/registerPaciente.interface';
 import { ApiService } from 'src/app/services/api.service';
 import Swal from 'sweetalert2';
-import { DialogComponent } from '../dialog/dialog.component';
+
 
 @Component({
   selector: 'app-dialog-pacientes',
@@ -16,15 +16,23 @@ export class DialogPacientesComponent implements OnInit {
   productForm !: FormGroup;
   actionBtn : string = "Guardar"
   hide = true;
+  folioCode = ['B','C','L','A','B']
+  folioCode2 = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O']
+  folioRandomCode = Math.floor(Math.random()*this.folioCode.length+1)
+  folioPositionCode = this.folioCode[this.folioRandomCode]
+  folioRandomCode2 = Math.floor(Math.random()*this.folioCode2.length+1)
+  folioPositionCode2 = this.folioCode2[this.folioRandomCode2]
+  folioRandomNumber = Math.floor(Math.random()*10000)
+
 
   constructor(private formBuilder : FormBuilder, 
     private api : ApiService, 
     @Inject(MAT_DIALOG_DATA) public editData : any,
-    private dialogRef : MatDialogRef<DialogComponent>) { }
+    private dialogRef : MatDialogRef<DialogPacientesComponent>) { }
 
   ngOnInit(): void {
     this.productForm = this.formBuilder.group({
-      folio: ["",Validators.required],
+      folio: ["BCL"+this.folioPositionCode+this.folioPositionCode2+this.folioRandomNumber,Validators.required],
       nombre : ['',Validators.required],
       apellidoP : ['',Validators.required],
       apellidoM: ['',Validators.required],
@@ -48,7 +56,7 @@ export class DialogPacientesComponent implements OnInit {
       }
     }
   
-    addPaciente(form:Register){
+    addPaciente(form:RegistroPaciente){
       if(!this.editData){
         if(this.productForm.valid){
           this.api.postPaciente(form).subscribe({
@@ -63,12 +71,14 @@ export class DialogPacientesComponent implements OnInit {
           })
         }
       }else{
-        this.updateUser(form)
+        this.editPaciente(form)
       }
     }
-    updateUser(form:Register){
-      console.log(this.productForm.get("code")?.value)
-      this.api.putUser(this.productForm.get("code")?.value, form).subscribe({
+  
+  
+    editPaciente(form:RegistroPaciente){
+      console.log(this.productForm.get("folio")?.value)
+      this.api.updatePaciente(this.productForm.get("folio")?.value, form).subscribe({
         next:(res)=>{
           Swal.fire('Exito','Se ha actualizado el usuario','success')
           this.productForm.reset();
@@ -78,6 +88,9 @@ export class DialogPacientesComponent implements OnInit {
           Swal.fire('Exito','Se ha producido un error al actualizar el usuario','error')
         }
       })
+    
     }
+
   }
+  
 
