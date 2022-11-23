7854b9e5abef2c4edaef7a2b0c4d18b0ec7b70d0
alexcas20 <alexcalvillosalas@gmail.com>
2022-11-22 23:10:30 -0600

    se agrego la interface de Registro de Pacientes

---
+++ src/app/models/registerPaciente.interface.ts
@@ -0,0 +1,11 @@
+export default interface RegistroPaciente{
+
+    folio: string,
+    nombre: string,
+    apellidoP: string,
+    apellidoM: string,
+    edad: any,
+    sexo: string,
+    telefono: string,
+    correo: string
+}
