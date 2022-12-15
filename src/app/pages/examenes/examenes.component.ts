615253a6bb8b29aafc81bb6a2ff63c148166c775
Hector Juvenal <hecjuvherr@hotmail.com>
2022-12-14 20:43:17 -0600

    componente examenes

---
+++ src/app/pages/examenes/examenes.component.ts
@@ -0,0 +1,93 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { ApiService } from 'src/app/services/api.service';
+import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { DialogPacientesComponent } from '../dialog-pacientes/dialog-pacientes.component';
+import Swal from 'sweetalert2';
+import { DialogComponent } from '../dialog/dialog.component';
+import { DialogExamenesComponent } from '../dialog-examenes/dialog-examenes.component';
+
+@Component({
+  selector: 'app-examenes',
+  templateUrl: './examenes.component.html',
+  styleUrls: ['./examenes.component.css'],
+})
+export class ExamenesComponent implements OnInit {
+  displayedColumns: string[] = [
+    'user',
+    'folio',
+    'paciente',
+    'medico',
+    'fecha',
+    'estatus',
+    'accion',
+  ];
+
+  dataSource!: MatTableDataSource<any>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor(private api: ApiService, private dialogPacientes: MatDialog) {}
+
+  ngOnInit(): void {
+    this.getDatosExamenes();
+  }
+
+  openDialogExamenes() {
+    this.dialogPacientes
+      .open(DialogExamenesComponent, {
+        width: '30%',
+      })
+      .afterClosed()
+      .subscribe((val) => {
+        if (val === 'save') {
+          this.getPacientes();
+        }
+      });
+  }
+
+  getAllUsers() {
+    this.api.getUsers().subscribe({
+      next: (res) => {
+        console.log(res);
+        this.dataSource = new MatTableDataSource(res);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+      },
+      error: (err) => {
+        Swal.fire('Error', 'No se han encontrado los usuarios', 'error');
+      },
+    });
+  }
+
+  getPacientes() {
+    this.api.getPacientes().subscribe({
+      next: (res) => {
+        console.log(res);
+        this.dataSource = new MatTableDataSource(res);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+      },
+      error: (err) => {
+        Swal.fire('Error', 'No se han encontrado los usuarios', 'error');
+      },
+    });
+  }
+
+  getDatosExamenes(){
+    this.getPacientes();
+    this.getAllUsers();
+  }
+  
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLocaleLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+}
