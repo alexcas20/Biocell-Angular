5152abd25fef133e00d4cbe0a7c59e4469a088e6
alexcas20 <alexcalvillosalas@gmail.com>
2023-01-10 14:51:51 -0600

    dialog examenes

--- src/app/pages/dialog-examenes/dialog-examenes.component.ts
+++ src/app/pages/dialog-examenes/dialog-examenes.component.ts
@@ -9,6 +9,7 @@ import Swal from 'sweetalert2';
 import { DialogBuscarPacienteComponent } from '../dialog-buscar-paciente/dialog-buscar-paciente.component';
 import { DialogComponent } from '../dialog/dialog.component';
 import { ServicioModalesService } from '../servicio-modales.service';
+import { DialogBuscarMedicoComponent } from '../dialog-buscar-medico/dialog-buscar-medico.component';
 
 @Component({
   selector: 'app-dialog-examenes',
@@ -16,10 +17,12 @@ import { ServicioModalesService } from '../servicio-modales.service';
   styleUrls: ['./dialog-examenes.component.css']
 })
 export class DialogExamenesComponent implements OnInit {
-  productForm!: FormGroup;
+   productForm!: FormGroup;
   actionBtn: string = 'Guardar';
   hide = true;
   cual: boolean= false
+  medico: boolean = false;
+  nombreMedico!: string;
 
   selectPaciente:any
   
@@ -30,9 +33,10 @@ export class DialogExamenesComponent implements OnInit {
   constructor(
     private formBuilder: FormBuilder,
     private api: ApiService,
-    @Inject(MAT_DIALOG_DATA) 
+    @Inject(MAT_DIALOG_DATA) public data:any, 
     private dialogRef: MatDialogRef<DialogComponent>,
     private dialogPacientes: MatDialog,
+    private DialogMedicos: MatDialog,
     private ServicioModal:ServicioModalesService
   ) { }
 
@@ -40,6 +44,7 @@ export class DialogExamenesComponent implements OnInit {
     this.productForm = this.formBuilder.group({
       folio: ['', Validators.required],
       nombre: ['', Validators.required],
+      nombreMedico: ['',Validators.required],
       apellidoP: ['', Validators.required],
       apellidoM: ['', Validators.required],
       especialidad:['',Validators.required],
@@ -49,13 +54,32 @@ export class DialogExamenesComponent implements OnInit {
       correo: ['', Validators.required],
     });
 
-    this.ServicioModal.getDatos(this.productForm)
-
-    if(this.ServicioModal.getDatos){
-    console.log("true")
-      this.productForm.controls['nombre'].setValue(this.ServicioModal.getDatos(this.productForm));
+    if(this.data){
+      this.productForm.controls['folio'].patchValue(this.data.folio)
+      this.productForm.controls['nombre'].patchValue(this.data.nombre)
+      
+      this.productForm.controls['apellidoP'].patchValue(this.data.apellidoP)
+      this.productForm.controls['apellidoM'].patchValue(this.data.apellidoM)
+      this.productForm.controls['edad'].patchValue(this.data.edad)
+      this.productForm.controls['sexo'].patchValue(this.data.sexo)
+      this.productForm.controls['telefono'].patchValue(this.data.telefono)
+      this.productForm.controls['correo'].patchValue(this.data.correo)
     }
+  
+    
+    
+
+    
+    
   }
+
+  
+
+
+
+  
+
+  
 
   BuscarPacientes(){
     this.dialogPacientes
@@ -68,7 +92,14 @@ export class DialogExamenesComponent implements OnInit {
           this.getPacientes();
         }
       });
+
+      this.dialogRef.close();
+
   }
+
+
+
+
 
   getPacientes() {
     this.api.getPacientes().subscribe({
@@ -77,16 +108,40 @@ export class DialogExamenesComponent implements OnInit {
         this.dataSource = new MatTableDataSource(res);
         this.dataSource.paginator = this.paginator;
         this.dataSource.sort = this.sort;
+      
+       
       },
       error: (err) => {
         Swal.fire('Error', 'No se han encontrado los usuarios', 'error');
       },
     });
+
+   
   }
 
+  buscarMedicos() {
+    
+    this.DialogMedicos.open(DialogBuscarMedicoComponent, {
+      width: '30%',
+    });
 
   
+    // if(this.ServicioModal.nombreMedico === '' || undefined){
+    //     alert('espera')
+    // }
 
+    this.traerDatos()
+   
+  }
 
+  traerDatos() {
+    
+    const medico = localStorage.getItem('medico')
+    const especialidad = localStorage.getItem('especialidad')
+      this.productForm.controls['nombreMedico'].patchValue(medico)
+      this.productForm.controls['especialidad'].patchValue(especialidad)
+      
+    
+  }
 
 }
