42eb4d6a39ddb055de2869fc189b57c677ac58cf
alexcas20 <alexcalvillosalas@gmail.com>
2023-01-10 14:52:13 -0600

    cambios servicio modal

--- src/app/pages/servicio-modales.service.ts
+++ src/app/pages/servicio-modales.service.ts
@@ -9,12 +9,47 @@ import { EventEmitter, Injectable, Output } from '@angular/core';
 
 export class ServicioModalesService {
   cual: boolean= false
+  folioPaciente:any;
+  nombrePaciente: any;
+  medico: boolean = false;
+
+  nombreMedico!:string;
+  especialidad!:string;
+  especialidadS: any;
+  nombreMedicoS: any;
+
+  localMedico!:any;
+  localEspecialidad!: any;
+
   constructor() { }
 
   getDatos(data:any){
       this.cual = true
-    alert("DATOS SETEADOS: " + data.nombre)
+      console.log("DATA: " + data)
+      this.folioPaciente = data.folio;
+      this.nombrePaciente = data.nombre;
 
+      console.log(`folio: ${this.folioPaciente} - nombre: ${this.nombrePaciente}`)
+
+
+    }
+
+    getDatosMedico(data:any){
+      console.log(JSON.stringify(data))
+      this.nombreMedicoS = data.nombreMedico
+      this.especialidadS = data.especialidad
+
+      this.getNombreMedico()
+     
+    }
+
+     getNombreMedico(){
+          this.nombreMedico = this.nombreMedicoS
+          this.especialidad = this.especialidadS
+          this.localMedico = localStorage.setItem('medico',this.nombreMedico);
+          this.localEspecialidad = localStorage.setItem('especialidad',this.especialidad)
+          console.log(this.nombreMedicoS);
+          console.log(this.especialidadS);
 
     }
   }
