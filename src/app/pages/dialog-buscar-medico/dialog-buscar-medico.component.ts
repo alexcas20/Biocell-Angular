d191443f6ba70e2f2f9fd561f5985d7c2b08de0e
alexcas20 <alexcalvillosalas@gmail.com>
2023-01-10 14:51:41 -0600

    dialog medico

--- src/app/pages/dialog-buscar-medico/dialog-buscar-medico.component.ts
+++ src/app/pages/dialog-buscar-medico/dialog-buscar-medico.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { ApiService } from 'src/app/services/api.service';
+import { DialogExamenesComponent } from '../dialog-examenes/dialog-examenes.component';
+import { DialogComponent } from '../dialog/dialog.component';
+import { ServicioModalesService } from '../servicio-modales.service';
+
 
 @Component({
   selector: 'app-dialog-buscar-medico',
@@ -7,9 +13,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DialogBuscarMedicoComponent implements OnInit {
 
-  constructor() { }
+  listarMedicos:any[] = []
+
+
+  constructor(private service: ApiService, private servicioModal: ServicioModalesService,
+    private dialogExamenes: MatDialog, private dialogRef: MatDialogRef<DialogComponent>,) { }
 
   ngOnInit(): void {
+
+    this.getMedicos()
+  }
+
+  getMedicos() {
+    this.service.getMedicos().subscribe( resp => this.listarMedicos = resp)
+  }
+
+
+  selectMedico(item:any){
+    console.log(item);
+    this.servicioModal.getDatosMedico(item);
+      
+
+     
+
+
+  
+    this.dialogRef.close();
   }
 
 }
